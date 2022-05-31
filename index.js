34abff6fa0e624628106de82a1da8e0266277b22
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-05-31 16:12:15 -0600

    moved functions to index

--- index.js
+++ index.js
@@ -1,11 +1,120 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const constructor = require("./src/construtor");
-const employeeChoice = require("./src/construtor");
-const manChoice = require("./src/construtor");
-const writeFile = require("./src/construtor");
+const { Employee, Engineer, Intern, Manager } = require("./src/construtor");
 
-manChoice().then(employeeChoice).then(writeFile);
+let teamMembers = [];
+
+const manChoice = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Who is the manager?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the manager ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the manager email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the manager office number?",
+        name: "office",
+      },
+    ])
+    .then((manInfo) => {
+      const { name, id, email, office } = manInfo;
+      const manager = new Manager(name, id, email, office);
+      teamMembers.push(manager);
+      console.log(manager);
+
+      employeeChoice(teamMembers);
+    });
+};
+
+const employeeChoice = (teamMembers) => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What type of employee would you like to add?",
+        name: "employeeType",
+        choices: ["Engineer", "Intern", "None"],
+      },
+      {
+        type: "input",
+        meassge: "What is the Employee name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        meassge: "What is the Employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        meassge: "What is the Employee email?",
+        name: "email",
+        when: (input) => input.employeeType === "Engineer",
+      },
+      {
+        type: "input",
+        meassge: "What is the Employee Github id?",
+        name: "github",
+        when: (input) => input.employeeType === "Engineer",
+      },
+      {
+        type: "input",
+        meassge: "What school does your intern attend?",
+        name: "school",
+        when: (input) => input.employeeType === "Intern",
+      },
+      {
+        type: "confirm",
+        message: "Do you have more employees to add? Yes or No",
+        name: "moreEmployees",
+      },
+    ])
+    .then((employeeInfo) => {
+      let { employeeType, name, id, email, github, school, moreEmployees } =
+        employeeInfo;
+
+      if (employeeType === "Engineer") {
+        let engineer = new Engineer(name, id, email, github);
+        teamMembers.push(engineer);
+        console.log(engineer);
+      } else {
+        let intern = new Intern(name, id, school);
+        teamMembers.push(intern);
+        console.log(intern);
+      }
+
+      if (moreEmployees) {
+        return employeeChoice(teamMembers);
+      } else {
+        return teamMembers;
+      }
+    });
+};
+
+const writeFile = (data) => {
+  fs.writeFile("./dist/index.html", data, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Your team profile has been created");
+    }
+  });
+};
+
+manChoice();
 
 // (module.exports = employeeChoice),
 //   manChoice,
--- src/construtor.js
+++ src/construtor.js
@@ -2,8 +2,6 @@ const inquirer = require("inquirer");
 const Choices = require("inquirer/lib/objects/choices");
 const fs = require("fs");
 
-const teamMembers = [];
-
 class Employee {
   constructor(name, id, email) {
     this.name = name;
@@ -24,6 +22,16 @@ class Employee {
   }
 }
 
+class Manager extends Employee {
+  constructor(name, id, email, office) {
+    // calling employee constructor
+    super(name, id, email);
+    this.officeNumber = office;
+  }
+  empType() {
+    return "Manager";
+  }
+}
 class Engineer extends Employee {
   constructor(name, id, email, github) {
     super(name, id, email);
@@ -54,131 +62,4 @@ class Intern extends Employee {
   }
 }
 
-class Manager extends Employee {
-  constructor(name, id, email, office) {
-    // calling employee constructor
-    super(name, id, email);
-    this.officeNumber = office;
-  }
-
-  // override employee role to manager
-  empType() {
-    return "Manager";
-  }
-}
-
-const manChoice = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Who is the manager?",
-        name: "name",
-      },
-      {
-        type: "input",
-        message: "What is the manager ID?",
-        name: "id",
-      },
-      {
-        type: "input",
-        message: "What is the manager email?",
-        name: "email",
-      },
-      {
-        type: "input",
-        message: "What is the manager office number?",
-        name: "office",
-      },
-    ])
-    .then((manInfo) => {
-      const { name, id, email, office } = manInfo;
-      const manager = new Manager(name, id, email, office);
-      teamMembers.push(manager);
-      console.log(manager);
-    });
-};
-
-const employeeChoice = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "list",
-        message: "What type of employee would you like to add?",
-        name: "employeeType",
-        choices: ["Engineer", "Intern"],
-      },
-      {
-        type: "input",
-        meassge: "What is the Employee name?",
-        name: "name",
-      },
-      {
-        type: "input",
-        meassge: "What is the Employee ID?",
-        name: "id",
-      },
-      {
-        type: "input",
-        meassge: "What is the Employee email?",
-        name: "email",
-        when: (input) => input.employeeType === "Engineer",
-      },
-      {
-        type: "input",
-        meassge: "What is the Employee Github id?",
-        name: "github",
-        when: (input) => input.employeeType === "Engineer",
-      },
-      {
-        type: "input",
-        meassge: "What school does your intern attend?",
-        name: "school",
-        when: (input) => input.employeeType === "Intern",
-      },
-      {
-        type: "confirm",
-        message: "Do you have more employees to add?",
-        name: "moreEmployees",
-        default: true,
-      },
-    ])
-    .then((employeeInfo) => {
-      let { employeeType, name, id, email, github, school, moreEmployees } =
-        employeeInfo;
-      let employee;
-
-      if (employeeType === "Engineer") {
-        employee = new Engineer(name, id, email, github);
-        console.log(employee);
-      } else {
-        employee = new Intern(name, id, school);
-        console.log(employee);
-      }
-
-      if (moreEmployees) {
-        return employeeChoice(teamMembers);
-      } else {
-        return teamMembers;
-      }
-    });
-};
-
-const writeFile = (data) => {
-  fs.writeFile("./dist/index.html", data, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      console.log("Your team profile has been created");
-    }
-  });
-};
-
-(module.exports = employeeChoice),
-  manChoice,
-  Employee,
-  Engineer,
-  Intern,
-  Manager,
-  writeFile;
+module.exports = { Employee, Engineer, Intern, Manager };
